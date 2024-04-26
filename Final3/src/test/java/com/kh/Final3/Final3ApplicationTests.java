9fe2d8a1958c49a4320405b58c88b87e68f4c5d1
Wing-Shal <152838103+Wing-Shal@users.noreply.github.com>
2024-04-26 13:41:34 +0900

    파이널3조 프로젝트 생성

---
+++ Final3/src/test/java/com/kh/Final3/Final3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.Final3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Final3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
