6f9f63bda16c8c8035a8d0995e05085f5f727072
SEOYEONHA <only3322@naver.com>
2024-04-29 15:52:36 +0900

    chat 관련 dao,dto,mapper 생성

--- Final3/src/main/java/com/kh/Final3/dao/MessageDao.java
+++ Final3/src/main/java/com/kh/Final3/dao/MessageDao.java
@@ -1,5 +1,7 @@
 package com.kh.Final3.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -12,11 +14,16 @@ public class MessageDao {
 	@Autowired
 	private SqlSession sqlSession;
 	
-	public MessageDto insert(MessageDto messageDto) {
-		int sequence = sqlSession.selectOne("message.sequence");
-		messageDto.setMessageNo(sequence);
-		sqlSession.insert("message.save", messageDto);
-		return sqlSession.selectOne("message.find", sequence);
+//	public MessageDto insert(MessageDto messageDto) {
+//		int sequence = sqlSession.selectOne("message.sequence");
+//		messageDto.setMessageNo(sequence);
+//		sqlSession.insert("message.save", messageDto);
+//		return sqlSession.selectOne("message.find", sequence);
+//	}
+	
+	//조회
+	public List<MessageDto> selectList(int chatroomNo){
+		return sqlSession.selectList("message.list", chatroomNo);
 	}
 	
 }
