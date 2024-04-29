029d97905b588abc1b1376e91cd4cc06429f8794
SEOYEONHA <only3322@naver.com>
2024-04-29 18:20:22 +0900

    메세지 전송자 이름 및 직책으로 설정, 시간설정

--- Final3/src/main/java/com/kh/Final3/dto/MessageDto.java
+++ Final3/src/main/java/com/kh/Final3/dto/MessageDto.java
@@ -1,18 +1,13 @@
 package com.kh.Final3.dto;
 
 import java.sql.Date;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import java.text.SimpleDateFormat;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 @Data @Builder @NoArgsConstructor @AllArgsConstructor
 public class MessageDto {
 	private int messageNo;
@@ -21,4 +16,13 @@ public class MessageDto {
 	private Date messageTime;
 	private int chatroomNo;
 	private int messageReply;
+	
+	private String messageSenderName;
+	private String messageSenderGrade;
+	
+    public String getMessageTimeMinute() {
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        return formatter.format(messageTime);
+    }
+
 }
--- Final3/src/main/java/com/kh/Final3/websocket/EmpWebsocketServer.java
+++ Final3/src/main/java/com/kh/Final3/websocket/EmpWebsocketServer.java
@@ -1,9 +1,23 @@
 package com.kh.Final3.websocket;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.socket.handler.BinaryWebSocketHandler;
 
+import com.kh.Final3.dao.MessageDao;
+import com.kh.Final3.dto.ChatroomDto;
+
 @Service
 public class EmpWebsocketServer extends BinaryWebSocketHandler{
+	
+	//사용자의 정보를 저장할 저장소 생성
+	private Map<String, ChatroomDto> chatroom = Collections.synchronizedMap(new HashMap<>());
+	
+	@Autowired
+	private MessageDao messageDao;
 
 }
