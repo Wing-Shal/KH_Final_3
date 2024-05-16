57f56729bded848b6b44a006d9c8ac01130b7803
godjiwon <jiwonis3@naver.com>
2024-05-16 09:43:14 +0900

    블라인드 게시판, 댓글 기능 구현

--- Final3/src/main/java/com/kh/Final3/dao/ReplyBlindDao.java
+++ Final3/src/main/java/com/kh/Final3/dao/ReplyBlindDao.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.kh.Final3.dto.BoardBlindDto;
 import com.kh.Final3.dto.ReplyBlindDto;
 
 @Repository
@@ -18,9 +19,10 @@ public class ReplyBlindDao {
 	public int sequence() {
 		return sqlSession.selectOne("replyBlind.sequence");
 	}
-	public void insert(ReplyBlindDto replyBlindDto) {
-		sqlSession.insert("replyBlind.save", replyBlindDto);
-	}
+	// 등록
+		public void insert(ReplyBlindDto replyBlindDto) {
+			sqlSession.insert("replyBlind.save", replyBlindDto);
+		}
 	
 	//조회
 	public List<ReplyBlindDto> selectList(){
@@ -40,5 +42,15 @@ public class ReplyBlindDao {
 		public boolean delete(int replyBlindNo) {
 			return sqlSession.delete("replyBlind.delete", replyBlindNo) > 0;
 		}
+		
+		// 토큰을 이용하여 사용자의 사원 번호와 회사 번호를 가져오는 메서드 추가
+	    public int selectCompanyNoByToken(String token) {
+	        return sqlSession.selectOne("boardBlind.selectCompanyNoByToken", token);
+	    }
+
+	    // 회사 번호를 이용하여 회사 이름을 가져오는 메서드 추가
+	    public String selectCompanyNameByCompanyNo(int companyNo) {
+	        return sqlSession.selectOne("boardBlind.selectCompanyNameByCompanyNo", companyNo);
+	    }
 	
 }
--- Final3/src/main/java/com/kh/Final3/restcontroller/ReplyBlindRestController.java
+++ Final3/src/main/java/com/kh/Final3/restcontroller/ReplyBlindRestController.java
@@ -10,16 +10,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.kh.Final3.dao.BoardBlindDao;
+import com.kh.Final3.dao.CompanyDao;
+import com.kh.Final3.dao.EmpDao;
 import com.kh.Final3.dao.ReplyBlindDao;
 import com.kh.Final3.dto.BoardBlindDto;
+import com.kh.Final3.dto.CompanyDto;
+import com.kh.Final3.dto.EmpDto;
 import com.kh.Final3.dto.ReplyBlindDto;
 import com.kh.Final3.service.JwtService;
-
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import com.kh.Final3.vo.LoginVO;
 
 @CrossOrigin
 @RestController
@@ -31,33 +36,51 @@ public class ReplyBlindRestController {
 	
 	@Autowired
 	private JwtService jwtService;
+	
+	@Autowired
+	private EmpDao empDao;
+	
+	@Autowired
+	private CompanyDao companyDao;
 
+	@Autowired
+	private BoardBlindDao boardBlindDao;
+	
 //	댓글 목록
 	@GetMapping("/")
 	public List<ReplyBlindDto> list(){
 		return replyBlindDao.selectList();
 	}
 	
-	//등록
-//		@PostMapping("/") 
-//		public ReplyBlindDto insert(@RequestBody ReplyBlindDto replyBlindDto, @RequestHeader("Authorization") String token){ 
-//			int sequence = replyBlindDao.sequence(); //번호생성
-////			String companyName = empDao.selectOnebyCompanyNo(token.getparsedCompanyNo())
-////			토큰 파싱해서 작성자 이름 가져오기
-//			String companyName = jwtService.parse(token.substring(7)).getAdminId();
-//			
-////			댓글이 달린 게시글 번호 가져오기ㄴ
-//			
-//			replyBlindDto.setReplyBlindNo(sequence); //시퀀스
-////			boardBlindDto.setBlindCom
-//			replyBlindDao.insert(replyBlindDto); //등록
-//			
-//			return replyBlindDao.selectOne(sequence);
-//		}
+	//requestBody 어노테이션 springFramework 제대로 쓰는지 꼭꼭꼭 확인
+	@PostMapping("/")
+	public ResponseEntity<ReplyBlindDto> insert(@RequestBody ReplyBlindDto replyBlindDto, @RequestHeader("Authorization") String token) {
+	    // 토큰을 해석하여 사용자 정보 가져오기
+	    LoginVO loginVO = jwtService.parse(token);
+	    int sequence = replyBlindDao.sequence();
+	    replyBlindDto.setReplyBlindNo(sequence);
+	    
+	    int loginId = loginVO.getLoginId(); // 토큰에서 사용자의 사원 번호 가져오기
+	    EmpDto empDto = empDao.selectOne(loginId); // 사용자의 사원 정보 조회
+	    int companyNo = empDto.getCompanyNo(); // 사용자의 회사 번호 가져오기
+	    System.out.println("회사번호"+companyNo);
+	    CompanyDto companyDto = companyDao.selectOne(companyNo); // 회사 정보 조회
+	    String companyName = companyDto.getCompanyName(); // 회사 이름 가져오기
+	    System.out.println("회사이름"+companyName);
+	    replyBlindDto.setReplyEmpNo(loginId); // 게시글 작성자의 사원 번호 설정
+	    System.out.println("작성자의 사원번호 설정"+replyBlindDto.getReplyEmpNo());
+	    replyBlindDto.setReplyBlindCompany(companyName); // 게시글 작성자의 회사 이름 설정
+	    System.out.println("작성자의 회사이름 설정"+replyBlindDto.getReplyBlindCompany());
+//	    여기까지 디버깅 완료
+	    replyBlindDao.insert(replyBlindDto); // 게시글 등록
+	    ReplyBlindDto insertedBoard = replyBlindDao.selectOne(replyBlindDto.getReplyBlindNo());
+	    System.out.println("replyBlindDto"+insertedBoard);
+	    return ResponseEntity.ok().body(insertedBoard);
+	}
 		
 //		삭제
 		//삭제
-		@DeleteMapping("/{blindNo}")
+		@DeleteMapping("/{replyBlindNo}")
 		public ResponseEntity<?> delete(@PathVariable int replyBlindNo){
 			boolean result = replyBlindDao.delete(replyBlindNo);
 			if(result == false) return ResponseEntity.notFound().build();
